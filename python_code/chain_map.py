5c051acd0664d665c3f073af1738ba325794c9f9
rumman07 <rummanahmed.ra@gmail.com>
2022-04-26 14:38:39 -0500

    learned and added code for using chainmap and python builin vars

--- python_code/chain_map.py
+++ python_code/chain_map.py
@@ -2,6 +2,6 @@ from collections import ChainMap
 
 dict = {"A": 1, "N": 2, "C": 4}
 dict1 = {"Z": 1, "D": 2, "P": 4}
-dict2 = {"W": 1, "E": 2, "R": 4}
-total = ChainMap(dict, dict1, dict2)
+dict2 = {"W": 1, "E": 2, "R": 4, "A":7}
+total = ChainMap(dict2, dict1, dict)
 print(total["A"])
---
+++ python_code/vars_example.py
@@ -0,0 +1,11 @@
+class DevOps():
+  def __init__(self):
+    self.tool = 'git'
+    self.methodology = 'agile practice'
+    self.automation = 'chef'
+    
+devops = DevOps()
+devops.tool = "k8s"
+list1 = [1,2,3,4,5]
+
+print(vars(devops))
